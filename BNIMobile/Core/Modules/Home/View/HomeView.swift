2bfb8e83c7057fdf6b4ecc24f441d4cc18d4b137
NaratamaIndo01 <muhammad.krismawan@indocyber.id>
2023-03-05 20:39:18 +0700

    Setup HomePage

---
+++ BNIMobile/Core/Modules/Home/View/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  BNIMobile
+//
+//  Created by Naratama on 05/03/23.
+//
+
+import UIKit
+
+
+class HomeView: UIViewController {
+    
+    enum Constants {
+        static let mainStoryboardName = "Home"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
