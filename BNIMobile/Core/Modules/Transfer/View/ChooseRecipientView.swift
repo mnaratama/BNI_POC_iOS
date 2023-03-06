87a0f400dbc3ffa8e2e815c703e389aa1f1e090c
Vicky <muhammad.julianto@indocyber.id>
2023-03-06 09:28:57 +0700

    - Add Transfer Module to the branch

---
+++ BNIMobile/Core/Modules/Transfer/View/ChooseRecipientView.swift
@@ -0,0 +1,12 @@
+//
+//  ChooseRecipientView.swift
+//  BNIMobile
+//
+//  Created by admin on 06/03/23.
+//
+
+import UIKit
+
+class ChooseRecipientViewController : UIViewController {
+    
+}
---
+++ BNIMobile/Core/Modules/Transfer/View/TransferView.swift
@@ -0,0 +1,20 @@
+//
+//  TransferView.swift
+//  BNIMobile
+//
+//  Created by admin on 06/03/23.
+//
+
+import UIKit
+
+class TransferView : UIViewController {
+    enum Constants {
+        static let mainStoryboardName = "Transfer"
+        static let chooseRecipientVC = "ChooseRecipientVC"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
