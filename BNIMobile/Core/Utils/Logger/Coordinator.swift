4f3a495cb19754139fadace4ff8a19b1d4704670
Hari R Krishna <hari.r.krishna@ibm.com>
2023-03-06 11:24:52 +0530

    basic project setup

---
+++ BNIMobile/Core/Utils/Logger/Coordinator.swift
@@ -0,0 +1,78 @@
+//
+//  Coordinator.swift
+//  BNIMobile
+//
+//  Created by Hari R Krishna on 04/03/23.
+//
+
+import UIKit
+
+protocol CoordinatorViewController: UIViewController {
+    var coordinator: Coordinator? { get set }
+}
+
+class Coordinator {
+    var root: UINavigationController
+    var childCoordinators = [Coordinator]()
+    var whenCompleted: (() -> Void)?
+
+    weak var firstVC: CoordinatorViewController?
+
+    init(root: UINavigationController) {
+        self.root = root
+    }
+
+//    final func start(animated: Bool = true) {
+//        let viewController = CustomerRegView()
+//        firstVC = viewController
+//
+//        guard let firstVC = firstVC else {
+//            fatalError("To Start VC you must override CustomerRegView before")
+//        }
+//
+//        firstVC.coordinator = self
+//        show(viewController: firstVC, animated: animated)
+//    }
+
+    func startInStack(at index: Int) {
+        let viewController = setFirstViewController()
+        firstVC = viewController
+
+        root.viewControllers.insert(viewController, at: index)
+    }
+
+    func setFirstViewController() -> CoordinatorViewController {
+        fatalError("Must be overridden in subclass")
+    }
+
+    func goBack() {
+        root.popViewController(animated: true)
+    }
+
+    func stop(animated: Bool = true) {
+        for viewController in root.viewControllers where viewController == firstVC {
+            root.popToViewController(viewController, animated: false)
+            root.popViewController(animated: animated)
+        }
+    }
+    
+    func removeFromStack() {
+        firstVC?.removeFromParent()
+    }
+    
+    func pop<T>(toScreenOfType: T.Type) where T: UIViewController {
+        if let foundVC = containsViewController(ofType: T.self) {
+            root.popToViewController(foundVC, animated: true)
+        }
+    }
+
+    func containsViewController<T>(ofType: T.Type) -> T? where T: UIViewController {
+        return root.viewControllers.first { viewController -> Bool in
+            viewController is T
+        } as? T
+    }
+
+    deinit {
+        logToConsole(message: "\(String(describing: self)) has been deinit")
+    }
+}
---
+++ BNIMobile/Core/Utils/Logger/Logger.swift
@@ -0,0 +1,14 @@
+//
+//  Logger.swift
+//  BNIMobile
+//
+//  Created by Hari R Krishna on 04/03/23.
+//
+
+import Foundation
+
+// MARK: - logging helpers
+
+func logToConsole(message: String) {
+    NSLog("BNI: \(message)")
+}
