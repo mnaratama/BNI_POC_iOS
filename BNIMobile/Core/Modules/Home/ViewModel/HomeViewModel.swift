aa445c888b7607284231b4af475d173c7c7d08ef
NaratamaIndo01 <muhammad.krismawan@indocyber.id>
2023-03-08 09:30:49 +0700

    Setup Model

---
+++ BNIMobile/Core/Modules/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  BNIMobile
+//
+//  Created by Naratama on 08/03/23.
+//
+
+import Foundation
+
+class HomeViewModel{
+    let homepageQuicklinkItems: [QuicklinkModel] = [QuicklinkModel(id: 1, title: "Transfer", image: "ic_optimize_cashflow"), QuicklinkModel(id: 2, title: "Top up & payment", image: "ic_mobile_add"), QuicklinkModel(id: 3, title: "TapCash", image: "ic_tapcash"), QuicklinkModel(id: 4, title: "QR Pay", image: "ic_qr_code")]
+}
