05e8323a19fe55991e29cf0533abb2c4f342480e
Greeshma Arunkumar <garunkum@in.ibm.com>
2023-03-08 21:57:37 +0530

    Added changes to handle the input validation and masking the password textfield

--- BNIMobile/Core/Modules/CustomerRegistration/View/EnterCredentialViewController.swift
+++ BNIMobile/Core/Modules/CustomerRegistration/View/EnterCredentialViewController.swift
@@ -13,16 +13,26 @@ class EnterCredentialViewController: BaseViewController {
     @IBOutlet weak var stackVW:UIStackView!
     @IBOutlet weak var lblUrerIdError:UILabel!
     @IBOutlet weak var lblPswdError:UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    @IBOutlet weak var userIdTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         hideErrorLabel()
+//        nextButton(shouldEnable: false)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
     }
     
+    fileprivate func nextButton(shouldEnable: Bool = false) {
+        self.nextButton.isEnabled = shouldEnable
+        self.nextButton.backgroundColor = shouldEnable ? UIColor(named: EnterMobileNumberViewController.Constants.colorBNITeal) : UIColor(named: EnterMobileNumberViewController.Constants.colorButtonDisabled)
+    }
+
     @IBAction func buttonNextTapped(_ sender: Any) {
         guard let viewController = UIStoryboard(name: StoryboardName.main, bundle: nil).instantiateViewController(withIdentifier: ViewControllerName.congratulationsDoneVC) as? CongratulationsDoneViewController else {
             fatalError("Failed to load Main from CongratulationsDoneViewController file")
--- BNIMobile/Core/Modules/CustomerRegistration/View/OtpViewController.swift
+++ BNIMobile/Core/Modules/CustomerRegistration/View/OtpViewController.swift
@@ -7,14 +7,56 @@
 
 import UIKit
 
-class OtpViewController: BaseViewController {
+class OtpViewController: BaseViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    var otp: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTextField()
+        nextButton(shouldEnable: false)
+    }
+    
+    /// Configures the text filed and adds the tap gesture
+    fileprivate func setupTextField() {
+        textField.isUserInteractionEnabled = true
+        textField.delegate = self
+        textField.addTarget(self, action: #selector(updatedValue(textField:)), for: .editingChanged)
+    }
+
+    /// method to enable / disable the next button
+    fileprivate func nextButton(shouldEnable: Bool = false) {
+        self.nextButton.isEnabled = shouldEnable
+        self.nextButton.backgroundColor = shouldEnable ? UIColor(named: EnterMobileNumberViewController.Constants.colorBNITeal) : UIColor(named: EnterMobileNumberViewController.Constants.colorButtonDisabled)
+    }
+
+    /// called each time user enters a value in the textFiled, Validation should happen here
+    @objc func updatedValue(textField: UITextField) {
+        guard let valueEntered = textField.text else {
+            return
+        }
+
+        otp = valueEntered
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard CharacterSet(charactersIn: "0123456789").isSuperset(of: CharacterSet(charactersIn: string)) else {
+            return false
+        }
+        
+        // to check the maxLength of otp = 6
+        let newString = ((textField.text ?? "") as NSString).replacingCharacters(in: range, with: string)
+        // enable next button only if the number is entered, disable if the number is cleared
+        self.nextButton(shouldEnable: !newString.isEmpty && newString.count >= 6)
+
+        return newString.count <= 6
     }
     
     @IBAction func nextButtonTapped(_ sender: Any) {
-        NetworkAccessLayer.shared.validateOTP(otp: "1111", completionHandler: { isSuccess, baseResponse, _  in
+        NetworkAccessLayer.shared.validateOTP(otp: otp, completionHandler: { isSuccess, baseResponse, _  in
             if isSuccess, let baseResponse = baseResponse {
                 print("Response JSON : \(baseResponse)")
             }
