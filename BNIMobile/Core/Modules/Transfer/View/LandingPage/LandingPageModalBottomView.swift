951be93f08aa6b148a5daf174268058a5a1bc517
Vicky <muhammad.julianto@indocyber.id>
2023-03-09 13:39:43 +0700

    Update 9 March 2023 13.39
    - Add LandingPageModal
    - Add TransferRecurringModal

---
+++ BNIMobile/Core/Modules/Transfer/View/LandingPage/LandingPageModalBottomView.swift
@@ -0,0 +1,12 @@
+//
+//  LandingPageModalBottomView.swift
+//  BNIMobile
+//
+//  Created by admin on 09/03/23.
+//
+
+import UIKit
+
+class LandingPageModalBottomView : UIViewController {
+    
+}
---
+++ BNIMobile/Core/Modules/Transfer/View/Transfer/TransferRecurringModalBottomView.swift
@@ -0,0 +1,12 @@
+//
+//  TransferRecurringModalBottomView.swift
+//  BNIMobile
+//
+//  Created by admin on 09/03/23.
+//
+
+import UIKit
+
+class TransferRecurringModalBottomView : UIViewController {
+    
+}
